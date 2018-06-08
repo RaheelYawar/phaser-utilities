81cc8f707b13754fa8fc1c19c5ce8a305e9f175b
Raheel Yawar <raheel@flying-sheep.com>
2018-06-08 18:12:49 +0200

    Added create overlay function.

--- lib6/PhaserUtility.js
+++ lib6/PhaserUtility.js
@@ -11,6 +11,23 @@ export default class PhaserUtility {
     }
 
     /**
+     * Create an half transparent overlay.
+     * @param {Object} game The current game context.
+     * @param {boolean} inputEnabled
+     * */
+    static createBackgroundOverlay(game, inputEnabled) {
+        const overlay = game.add.graphics(0, 0);
+        overlay.name = `overlay_${Date.now()}`;
+        overlay.beginFill(0x000000);
+        overlay.drawRect(0, 0, game.width, game.height);
+        overlay.endFill();
+        overlay.alpha = 0.5;
+        overlay.inputEnabled = inputEnabled;
+
+        return overlay;
+    }
+
+    /**
      * @param {number} min Minimum inclusive integer.
      * @param {number} max Maximum inclusive integer.
      * */
